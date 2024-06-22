35dd4e7a632ac5a1ccfbe0e1383a4b0a03e5746a
JxxHxxx <leesin5498@naver.com>
2024-06-22 17:50:45 +0900

    RFTR 페이지네이션 컴포넌트 Prop 주석 및 고정 값 선언

--- src/component/button/PaginationButtons.tsx
+++ src/component/button/PaginationButtons.tsx
@@ -7,7 +7,7 @@ interface PaginationButtonsProp {
     totalPages: number;
     selectedNum: number;
     showOnePageButtonAmount: number;
-    sendSelectedNumCallback: (nowNum: number) => any;
+    sendSelectedNumCallback: (nowNum: number) => void;
 }
 
 interface paginationState {
@@ -16,18 +16,19 @@ interface paginationState {
 }
 
 export default function PaginationButtons({
-    pageNums = [],
-    totalPages = 0,
-    selectedNum = 0,
-    showOnePageButtonAmount = 0,
-    sendSelectedNumCallback }: PaginationButtonsProp) {
+    pageNums = [], // 현재 페이지에서 보여줄 버튼 번호들
+    totalPages = 0, // 총 페이지
+    selectedNum = 0, // 현재 선택한 버튼의 번호
+    showOnePageButtonAmount = 0, // 한 페이지에 보여줄 버튼의 양
+    sendSelectedNumCallback // 콜백함수, 현재 선택한 버튼의 번호를 상위 컴포넌트로 전이
+}: PaginationButtonsProp) {
 
     const [pagination, setPagination] = useState<paginationState>({
         pageNumsState: pageNums,
         selectedNumState: selectedNum,
     });
-
-    const handleNextButtons = () => {
+    // '>' 버튼을 눌렀을 때 이벤트 
+    const handleNextButton = () => {
         if (pagination.selectedNumState + showOnePageButtonAmount > totalPages) {
             return
         }
@@ -39,8 +40,8 @@ export default function PaginationButtons({
 
         sendSelectedNumCallback(pagination.selectedNumState + showOnePageButtonAmount);
     }
-
-    const handlePreviousButtons = () => {
+    // '<' 버튼을 눌렀을 때 이벤트 
+    const handlePreviousButton = () => {
         if (pagination.selectedNumState - showOnePageButtonAmount <= 0) {
             return;
         }
@@ -63,13 +64,13 @@ export default function PaginationButtons({
 
 
     return <Fragment>
-        <Button className={"bs"} name={"<"} onClick={handlePreviousButtons} />
+        <Button className={"bs"} name={"<"} onClick={handlePreviousButton} />
         {pagination.pageNumsState
             .filter(nowNum => nowNum <= totalPages)
             .map(nowNum => <Button
                 className={nowNum === pagination.selectedNumState ? "bs_selected" : "bs"}
                 name={nowNum}
                 onClick={() => handleClickPageNumButton(nowNum)} />)}
-        <Button className={"bs"} name={">"} onClick={handleNextButtons} />
+        <Button className={"bs"} name={">"} onClick={handleNextButton} />
     </Fragment>
 }
--- src/page/message/history/MessageHistContent.tsx
+++ src/page/message/history/MessageHistContent.tsx
@@ -4,6 +4,9 @@ import PaginationButtons from "../../../component/button/PaginationButtons";
 import { getMessageQResult } from "../../../api/MessageApi";
 import { useEffect, useState } from "react";
 
+const showOnePageMessageResultAmount:number = 10; // 한 페이지에 보여줄 이력의 갯수
+const showOnePageButtonAmount:number = 5; // 페이지에서 보여줄 버튼의 갯수
+
 export default function MessageHistContent() {
     const [messageQReuslt, setMessageQResult] = useState<any>({
         pageable: {
@@ -13,9 +16,9 @@ export default function MessageHistContent() {
 
     const requestToServer = async () => {
         const params = {
-            startDate: '2024-06-21',
-            endDate: '2024-06-21',
-            size: 5,
+            startDate: '2024-06-22',
+            endDate: '2024-06-22',
+            size: showOnePageMessageResultAmount,
             page: messageQReuslt.pageable.pageNumber // 현재 페이지 + 1 = 버튼 숫자
         }
         const response = await getMessageQResult(params);
@@ -32,8 +35,6 @@ export default function MessageHistContent() {
         }))
     }
 
-    console.log('result', messageQReuslt.content);
-
     useEffect(() => {
         requestToServer();
     }, [messageQReuslt.pageable.pageNumber])
@@ -44,23 +45,32 @@ export default function MessageHistContent() {
         <tr>
             <td>PK</td>
             <td>목적지</td>
+            <td>의뢰자 ID</td>
+            <td>의뢰자 명</td>
+            <td>처리 유형</td>
             <td>처리시작시간</td>
             <td>처리종료시간</td>
+            <td>처리 상태</td>
+
         </tr>
         {messageQReuslt.content && messageQReuslt.content.map((content: any) => <>
-            <tr>
+            <tr key={content.pk}>
                 <td>{content.pk}</td>
                 <td>{content.messageDestination}</td>
+                <td>{content.body.requester_id}</td>
+                <td>{content.body.requester_name}</td>
+                <td>{content.messageProcessType}</td>
                 <td>{content.processStartTime}</td>
                 <td>{content.processEndTime}</td>
+                <td>{content.messageProcessStatus}</td>
             </tr>
 
         </>)}
         <PaginationButtons
             sendSelectedNumCallback={(pageNumber: number) => updatePageNumber(pageNumber)}
             totalPages={messageQReuslt.totalPages}
-            pageNums={Array.from({ length: 5 }, (_, index) => val + index)}
+            pageNums={Array.from({ length: showOnePageButtonAmount }, (_, index) => val + index)}
             selectedNum={selectedNum}
-            showOnePageButtonAmount={5} />
+            showOnePageButtonAmount={showOnePageButtonAmount} />
     </Fragment>
 }
