cd0690ee60da616f41b5e7053d56dba6d11c6001
JxxHxxx <leesin5498@naver.com>
2024-11-11 15:16:08 +0900

    FIX 아이디/패스워드 오류 시 정상적으로 응답 받도록 변경

--- src/api/AuthApi.ts
+++ src/api/AuthApi.ts
@@ -16,7 +16,7 @@ const SignIn = (requestBody: object) => {
 
     return instance.post(`/api/auth/login`, requestBody)
         .then(res => res)
-        .catch(err => err)
+        .catch(err => err.response)
 }
 
 
